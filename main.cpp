adb78d954d960a22d0290330bf79f0f3b135bdc7
avros16 <114757828+avros16@users.noreply.github.com>
2024-05-06 12:20:51 +0000

    changing index

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 // indexes start at 0
 //you dont have to add the size into the []
-
+// elemts are inside []
 
 
 int main() {
   
   int x_arr[6] = {5,10,7,4,10,8};
-
+ x_arr[0] = 6;
 
   for (int ct{0}; ct <=5; ct++){
   cout << "value of x is " <<x_arr[ct]<<endl; 
